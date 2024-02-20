9c17a0d01b84a5e7183365861b8004b089055481
Clyde Limbaco <kuraido.eiron@gmail.com>
2024-02-20 19:25:56 +0800

    commit

--- Connection/index.php
+++ Connection/index.php
@@ -1,10 +1,15 @@
 <?php 
     class Connection {
         public $connect; 
-        public $username = "root";
+        // public $username = "root";
+        // public $host = "localhost";
+        // public $password = "";
+        // public $database = "dom";
+
+        public $username = "u653188733_acaduser";
         public $host = "localhost";
-        public $password = "";
-        public $database = "dom";
+        public $password = "!Acadpassword123";
+        public $database = "u653188733_acadrecords";
 
         public function connects() {
             return $this-> connect = mysqli_connect($this -> host, $this -> username, $this -> password, $this -> database);
